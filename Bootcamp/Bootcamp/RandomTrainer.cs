ddd7c0ad93188bf0b4452f0e746ed6f97c029908
james_bob <james-bob@mail.ru>
2018-12-18 21:38:21 +0300

    добавление класса непредсказуемых

---
+++ Bootcamp/Bootcamp/RandomTrainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    public class RandomTrainer : Traider
+    {
+        static int count;
+        public RandomTrainer()
+            : base()
+        {
+            count++;
+            name = "Непредсказуемый_" + count;
+        }
+
+        protected override TraideSolution Solution
+        {
+            get
+            {
+                TraideSolution solution = (TraideSolution)rnd.Next(2);
+                if (rnd.Next(100) < 5)
+                {
+                    return (TraideSolution)(1 - solution);
+                }
+                else
+                {
+                    return solution;
+                }
+            }
+        }
+    }
+}
