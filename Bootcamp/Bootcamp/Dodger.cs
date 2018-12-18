a8079c7fc6abfca7ff1aeb83c2d9fe007ffad9d1
james_bob <james-bob@mail.ru>
2018-12-18 21:40:37 +0300

    класс хитрецов

---
+++ Bootcamp/Bootcamp/Dodger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    public class Dodger : Traider
+    {
+        static int count;
+        public Dodger()
+            : base()
+        {
+            count++;
+            name = "Хитрец_" + count;
+        }
+
+        protected override TraideSolution LastOpponentSolution
+        {
+            get
+            {
+                return base.LastOpponentSolution;
+            }
+            set
+            {
+                base.LastOpponentSolution = value;
+            }
+        }
+
+        protected override TraideSolution Solution
+        {
+            get
+            {
+                if (rnd.Next(100) < 5)
+                {
+                    return (TraideSolution)(1-LastOpponentSolution);
+                }
+                else
+                {
+                    return LastOpponentSolution;
+                }
+            }
+        }
+    }
+}
